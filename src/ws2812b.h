c63b569fd95f1fcec696222be96b212ba5989a60
stric9r <76652407+stric9r@users.noreply.github.com>
2023-11-26 16:50:45 -0500

    Add missing file left out by mistake
    Add general include file, accidently left it out

---
+++ src/ws2812b.h
@@ -0,0 +1,12 @@
+/// ws2812b
+///
+/// This is the include file to work with the ws2812b led strips
+
+#ifndef WS2812B_H_
+#define WS2812B_H_
+
+#include "ws2812b_data.h"
+#include "ws2812b_draw_common.h"
+#include "ws2812b_draw.h"
+
+#endif /* WS2812B_H_ */
